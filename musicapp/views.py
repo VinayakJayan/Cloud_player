0915504f7cd723182fc2b588bafe2537812e713d
unknown <jvinayak912@gmail.comgit config --global user.name VinayakJayan>
2024-11-03 09:32:59 +0530

    updated the views.py

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Song, Album
 
-# Create your views here.
+def song_list(request):
+    songs = Song.objects.all()
+    return render(request, 'song_list.html', {'songs': songs})
+
+def song_detail(request, pk):
+    song = Song.objects.get(pk=pk)
+    return render(request, 'song_detail.html', {'song': song})
